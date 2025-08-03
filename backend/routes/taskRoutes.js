a65b1348410dc074fff6019ccfc267deccd4e65f
Khang Vo <volenhatkhang1808@gmail.com>
2025-08-03 22:16:33 +1000

    taskController.js file is added

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,10 @@
-
 const express = require('express');
-const { getTasks, addTask, updateTask, deleteTask } = require('../controllers/taskController');
+const {
+	getTasks,
+	addTask,
+	updateTask,
+	deleteTask,
+} = require('../controllers/taskController');
 const { protect } = require('../middleware/authMiddleware');
 const router = express.Router();
 
